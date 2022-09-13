0bf43d89efb217150203b7434faddbf98ae05f18
Yamil Villegas <YTorres87@gmail.com>
2022-09-13 16:41:02 -0400

    The App Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React from 'react';
 import './App.css';
+import { Container, Navbar, NavbarBrand } from 'reactstrap';
+import RetailplusLogo from './app/assets/img/retailpluslogo.png';
 
 function App() {
   return (
     <div className="App">
-      hello retailplus!
+      <Navbar dark color='primary' sticky='top' expand='md'>
+        <Container>
+          <NavbarBrand>
+            <img src={RetailplusLogo} alt='retailplus logo' />
+          </NavbarBrand>
+        </Container>
+      </Navbar>
+      Just getting started!
     </div>
   );
 }
