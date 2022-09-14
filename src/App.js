f7f792c81e84f33ad11e3825810186a31e1b3e0e
Yamil Villegas <YTorres87@gmail.com>
2022-09-14 17:11:44 -0400

    Footer component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import './App.css';
 import EventsList from './features/specialevents/EventsList';
 import Header from './components/Header';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <EventsList />
+      <Footer />
     </div>
   );
 }
